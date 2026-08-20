1d2f9f4cb2a05e29d47938a95afd4121ed28e3b8
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-08-20 12:09:09 +0200

    Fix output preview image sizing

--- Components/UserControls/ColorChannelOutput.xaml.cs
+++ Components/UserControls/ColorChannelOutput.xaml.cs
@@ -22,7 +22,7 @@ namespace ImageCombinerChannelExtractor.Components.UserControls
                 return;
             }
 
-            crdPanel.Content = GetBtnString(channel);
+            btnDownloadChannel.Content = GetBtnString(channel);
             base.UpdateColoringStuff(channel);
         }
 
